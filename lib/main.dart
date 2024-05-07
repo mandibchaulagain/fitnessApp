e79ffc1f1eca46d6fc63e594472c50c1765bb2c0
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-07 21:34:46 +0545

    removed the debug sign

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,//removes the debug sign on top-right
       home: Homepage()
     );
   }
