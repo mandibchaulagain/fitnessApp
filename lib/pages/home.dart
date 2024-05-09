35fe39fdf577f154e451e346c618dd72c3544585
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-09 21:43:48 +0545

    added the folder last time, so made it right

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,13 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      // adding this because at the top of design we have an appBar
+      //appBar is property and AppBar() is a widget
+    );
+    //Scaffold is a class in flutter which provides many widgets like appbar..
+    //it will occupy a part or the whole screen
+    
   }
 }
