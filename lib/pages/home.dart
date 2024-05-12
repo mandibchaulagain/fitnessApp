27a4d06bccbac93d3a13d7de80fe33e232917e8f
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-12 21:27:03 +0545

    used action property for the right button, used the same code of the left arrow button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,6 +36,24 @@ class Homepage extends StatelessWidget {
             borderRadius: BorderRadius.circular(10)
           ),
         ),
+        actions: [
+          Container(
+          // width: 30,
+          // height: 30, //as seen this won't change the size
+          margin: EdgeInsets.all(10),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+          //despite the above adding the pic, we don't see it
+          //this is coz the color of icon and container is black
+          alignment: Alignment.center,//this and down two's , tis the
+          //most IMPORTANT as only becoz of this the below two work
+          height: 20,//here
+          width: 20, //and here together make the icon smaller
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),//showed the icon
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        ],
       ),
       // adding this because at the top of design we have an appBar
       //appBar is property and AppBar() is a widget
